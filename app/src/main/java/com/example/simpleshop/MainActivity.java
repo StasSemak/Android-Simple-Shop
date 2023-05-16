5e800d55a516f741465c65b465e86676cc6bffea
StasSemak <semaks051@gmail.com>
2023-05-16 18:38:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simpleshop/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.simpleshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.simpleshop.application.HomeApplication;
+import com.example.simpleshop.category.CategoriesAdapter;
+import com.example.simpleshop.constants.Urls;
+import com.example.simpleshop.dto.category.CategoryItemDTO;
+import com.example.simpleshop.service.CategoryNetwork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    CategoriesAdapter adapter;
+    RecyclerView rc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView avatar = (ImageView) findViewById(R.id.myImage);
+        String url = Urls.BASE + "/images/azf5dmf2.ua2.jpg";
+        Glide.with(HomeApplication.getAppContext())
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(avatar);
+
+        rc = findViewById(R.id.rcvCategories);
+        rc.setHasFixedSize(true);
+        rc.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false));
+        rc.setAdapter(new CategoriesAdapter(new ArrayList<>()));
+
+        requestServer();
+    }
+
+    void requestServer() {
+        CategoryNetwork.getInstance()
+                .getJsonApi()
+                .list()
+                .enqueue(new Callback<List<CategoryItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
+                        List<CategoryItemDTO> list = response.body();
+                        adapter = new CategoriesAdapter(list);
+                        rc.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CategoryItemDTO>> call, Throwable t) {
+
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/simpleshop/constants/Urls.java
@@ -0,0 +1,5 @@
+package com.example.simpleshop.constants;
+
+public class Urls {
+    public static final String BASE = "http://android-api.somee.com";
+}
